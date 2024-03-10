6f5fce6bccf081e6a242f8014f8558d73a257d8c
bishalbera <bishalbera2@gmail.com>
2024-03-10 09:34:09 +0530

    added func to insert the datas

--- db/phone.go
+++ db/phone.go
@@ -2,12 +2,12 @@ package db
 
 import (
 	"database/sql"
-	
+
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 type Phone struct {
-	ID int
+	ID     int
 	Number string
 }
 
@@ -31,3 +31,45 @@ func createDB(db *sql.DB, name string) error {
 	}
 	return nil
 }
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS" + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func (db *DB) Seed() error {
+	data := []string{
+		"1234567890",
+		"123 456 7891",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"123-456-7890",
+		"1234567892",
+		"(123)456-7892",
+	}
+
+	for _, number := range data {
+		if _, err := insertPhone(db.db, number); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+
--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"unicode"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
+	}
+
+	dbUser := os.Getenv("user")
+	dbPassword := os.Getenv("password")
+	dbHost := os.Getenv("host")
+    dbPort := os.Getenv("port")
+	dbName := os.Getenv("dbname")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword)
 	
+
 }
 
 func normalizeNumber(phone string) string {
@@ -18,4 +37,10 @@ func normalizeNumber(phone string) string {
 		}
 	}
 	return normalizedNumber.String()
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
